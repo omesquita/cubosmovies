36f12daa0c7009db33e125b48f359068d16d61b9
Osnir <contato@osnirmesquita.com.br>
2019-06-03 09:21:04 -0300

    add retrofit dependicies

--- app/src/main/java/br/com/osnirmesquita/cubosmovies/App.kt
+++ app/src/main/java/br/com/osnirmesquita/cubosmovies/App.kt
@@ -1,4 +1,21 @@
 package br.com.osnirmesquita.cubosmovies
 
-class App {
+import android.app.Application
+import org.koin.core.context.startKoin
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+        startKoin {
+
+        }
+    }
 }
--- app/src/main/java/br/com/osnirmesquita/cubosmovies/di/Modules.kt
+++ app/src/main/java/br/com/osnirmesquita/cubosmovies/di/Modules.kt
@@ -1,2 +1,10 @@
 package br.com.osnirmesquita.cubosmovies.di
 
+import org.koin.dsl.module
+
+
+private const val BASE_URL = "https://api.themoviedb.org/3/"
+
+val networkModule = module{
+
+}
