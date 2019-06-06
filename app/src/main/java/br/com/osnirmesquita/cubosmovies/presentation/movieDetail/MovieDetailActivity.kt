7880f29fa0adf04fafbb7cc22f4bd72418879595
Osnir Mesquita <contato@osnirmesquita.com.br>
2019-06-06 01:20:32 -0300

    create movie detail

---
+++ app/src/main/java/br/com/osnirmesquita/cubosmovies/presentation/movieDetail/MovieDetailActivity.kt
@@ -0,0 +1,13 @@
+package br.com.osnirmesquita.cubosmovies.presentation.movieDetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.osnirmesquita.cubosmovies.R
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
