0fadbb56020961543039aca9c4e86a98ea14a988
Osnir Mesquita <contato@osnirmesquita.com.br>
2019-06-05 21:19:17 -0300

    Fix instantiation of presenter to base activity

--- app/src/main/java/br/com/osnirmesquita/cubosmovies/presentation/base/BaseActivity.kt
+++ app/src/main/java/br/com/osnirmesquita/cubosmovies/presentation/base/BaseActivity.kt
@@ -4,34 +4,44 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import timber.log.Timber
-import java.lang.ClassCastException
 
-abstract class BaseActivity<T, V> : AppCompatActivity(), BaseContract.View
-        where T : BaseContract.Presenter<V>,
+abstract class BaseActivity<P, V> : AppCompatActivity(), BaseContract.View
+        where P : BaseContract.Presenter<V>,
               V : BaseContract.View {
 
-    abstract fun getPresenter(): T
+    /**
+     * Field to store the presenter in the all activities
+     * */
+    protected var presenter: P? = null
+        //set presenter only if none be have instantiated
+        set(value) {
+            if (field == null) {
+                field = value
+            }
+        }
 
-    abstract fun destroyPresenter()
+    /**
+     *
+     * Function to forces the activity to extend the class to make the presenter bind
+     *
+     * @return [P] type to presenter
+     * */
+    protected abstract fun bindPresenter(): P
 
     @Suppress("UNCHECKED_CAST")
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-
-        try {
-            getPresenter().attachView(this as V)
-        } catch (e: ClassCastException) {
-            Timber.e(e)
-        }
-
-
         super.onCreate(savedInstanceState, persistentState)
+        Timber.d("onCreate()")
+        //When starting onCreate calls the function to create Presenter and attach the view
+        presenter = bindPresenter()
+        presenter?.attachView(this as V)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Timber.d("onDestroy()")
-        getPresenter().detachView()
-        getPresenter().destroyView()
-        destroyPresenter()
+        presenter?.detachView()
+        presenter?.destroyView()
+        presenter = null
     }
 }
